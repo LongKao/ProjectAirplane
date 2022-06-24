d8b74b092ce3ea8a2f2b6e2ff755c916b441b680
LongKao <61633084+LongKao@users.noreply.github.com>
2022-06-23 20:21:14 -0400

    Update PayRequest.js

--- components/PayRequest.js
+++ components/PayRequest.js
@@ -62,7 +62,15 @@ const PayRequest = (props) => {
 
   return (
     <View style={styles.view}>
-      <TouchableOpacity style={styles.payReq} onPress={()=>{addItem(props.name,props.amount,props.reason),props.navigation.navigate('Home');}}>
+      <TouchableOpacity style={styles.payReq} onPress={()=>{
+        if(props.name==='' || props.amount===0 || isNaN(props.amount)==true || props.reason===''){
+          Alert.alert('Uh oh', 'Please enter correct information', [{ text: "okie dokie", onPress: () => console.log("OK Pressed") }],{ cancelable: true });
+        }
+        else{
+          addItem(props.name,props.amount,props.reason),
+          props.navigation.navigate('Home');
+        }
+      }}>
           <Text style={styles.text}>Send money</Text>
           <Icon name="paper-plane" color="#fff" />
       </TouchableOpacity>
